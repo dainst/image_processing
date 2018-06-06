c5cf8496e2f77cf39205b0ffb6d7f897513e0fd5
Simon Hohl <simon.hohl@dainst.org>
2018-06-06 11:58:47 +0200

    Refactor web preview

--- group_images.py
+++ group_images.py
@@ -1,5 +1,6 @@
 import sys
 import pickle
+import json
 
 import numpy as np
 
@@ -11,10 +12,14 @@ def group_images(data, output_directory, neighbour_count):
     counter = 1
     count = len(data)
 
+    file_list = []
+
     for (file_name, features) in data:
         print(f"Evaluating neighbours for {file_name} (#{counter}/{count}).")
         counter += 1
         neighbours = []
+
+        file_list.append(file_name)
 
         for(neighbour_name, neighbour_features) in data:
             if neighbour_name == file_name:
@@ -37,6 +42,9 @@ def group_images(data, output_directory, neighbour_count):
             pickle.dump(result, output_file)
 
         create_json.write_as_json(result, output_directory, neighbour_count)
+
+    with open(f"{output_directory}/images_list.json", 'w') as image_list_file:
+        json.dump(file_list, image_list_file)
 
 
 if __name__ == '__main__':
