103cb5878daa9255fe678678242879a52856f102
Simon Hohl <simon.hohl@dainst.org>
2018-06-08 11:42:58 +0200

    Refactor feature extraction
    Images are processed in batches of 10000 images. Each batch yields two files:
    
    1) The features in an `m x n` numpy matrix (Pickle), where `m` is the number of images and `n` contains the images index `i` followed 2048 feature values.
    2) The image names as a list (JSON), where each list index corresponds to one image index `i` in the matrix.

--- create_image_features.py
+++ create_image_features.py
@@ -1,9 +1,15 @@
 import sys
 import os
 import pickle
+import json
+import logging
 
 import tensorflow as tf
 import numpy as np
+
+logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def create_graph():
@@ -15,15 +21,13 @@ def create_graph():
 
 def create_features(image_list):
 
-    create_graph()
-
-    result = []
+    result_name_mapping = []
+    result_features = []
 
     with tf.Session() as sess:
-
         for image_idx, image in enumerate(image_list):
             try:
-                print(f"Parsing {image}, (#{image_idx})...")
+                logger.debug(f"Parsing {image}, (#{image_idx})...")
                 if not tf.gfile.Exists(image):
                     tf.logging.fatal(f'File does not exist {image}.')
 
@@ -34,29 +38,64 @@ def create_features(image_list):
                     feature_set = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
                     feature_vector = np.squeeze(feature_set)
 
-                    print(feature_vector.shape)
-
-                    result.append((os.path.basename(image), feature_vector))
+                    result_name_mapping.append(os.path.basename(image))
+                    current_index = len(result_name_mapping) - 1
+                    result_features.append(np.append([current_index], feature_vector))
 
             except Exception as e:
-                print(e)
+                logger.error(e)
+                logger.error(image)
 
-    return result
+    return result_name_mapping, result_features
+
+
+def process_file_list(image_list, path, batch_count):
+    logger.debug(len(image_list))
+
+    (name_list, features) = create_features(image_list=image_list)
+    batch_path = f'{path}{batch_count}/'
+
+    logger.info(f"Writing processed batch {batch_count}.")
+
+    if not os.path.exists(os.path.dirname(batch_path)):
+        os.makedirs(os.path.dirname(batch_path))
+
+    with open(batch_path + '/features.pickle', 'wb') as output_file:
+        pickle.dump(features, output_file)
+
+    with open(batch_path + '/image_mapping.json', 'w') as output_file:
+        json.dump(name_list, output_file)
 
 
 if __name__ == '__main__':
     image_root = sys.argv[1]
+    output_root = sys.argv[2]
+
+    if not output_root.endswith('/'):
+        output_root += '/'
 
     file_list = []
 
+    image_counter = 0
+    batch_counter = 0
+
+    logger.info('Creating graph.')
+    create_graph()
+
+    logger.info('Starting to process images...')
     for root, dirs, files in os.walk(image_root):
         for file in files:
             if file.endswith('.jpg'):
                 file_list.append(os.path.abspath(f"{root}/{file}"))
+                image_counter += 1
 
-    features = create_features(image_list=file_list)
+            if image_counter == 10000:
+                process_file_list(file_list, output_root, batch_counter)
+                batch_counter += 1
+                image_counter = 0
+                file_list = []
 
-    with open(sys.argv[2] + '/features.pickle', 'wb') as file:
-        pickle.dump(features, file)
+    if len(file_list) != 0:
+        process_file_list(file_list, output_root, batch_counter)
 
-    print('Done.')
+    logger.info('Done.')
