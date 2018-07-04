4ed59ae01b4142d52f621f2c3b8376af85939e6a
Simon Hohl <simon.hohl@dainst.org>
2018-07-04 16:30:16 +0200

    Adjust neighbours script
    Now writing results to MariaDB instead to disk.

--- db/mariadb.py
+++ db/mariadb.py
@@ -1,6 +1,7 @@
 import logging
 import MySQLdb
 import json
+import numpy
 
 logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -45,6 +46,53 @@ def write_file_features(file_name, features):
     global connection
     statement = 'INSERT IGNORE INTO `image_features` (`image_id`, `features`) ' \
                 'VALUES ((SELECT `id` FROM `image_names` WHERE `file_name` = "' + file_name + '"), "' + json.dumps(features.tolist()) +'");'
+
+    cursor = connection.cursor()
+    cursor.execute(statement)
+    connection.commit()
+    cursor.close()
+
+
+def get_feature_count():
+    global connection
+    statement = 'SELECT count(*) FROM `image_features`;'
+
+    cursor = connection.cursor()
+    cursor.execute(statement)
+    result = cursor.fetchone()
+
+    return result[0]
+
+
+def _get_feature_batch_data(offset, size):
+    global connection
+    statement = 'SELECT * FROM `image_features` WHERE `image_id` >= ' + str(offset) + ' AND `image_id` < ' + str(offset + size) + ';'
+
+    cursor = connection.cursor()
+    cursor.execute(statement)
+
+    result = cursor.fetchall()
+
+    return result
+
+
+def get_feature_batch(offset, batch_size):
+    batch_data = _get_feature_batch_data(offset, batch_size)
+    batch_feature_vectors = []
+    batch_feature_ids = []
+    for (image_id, feature_string) in batch_data:
+        batch_feature_vectors.append(json.loads(feature_string))
+        batch_feature_ids.append(image_id)
+
+    matrix = numpy.array(batch_feature_vectors)
+
+    return batch_feature_ids, matrix
+
+
+def write_neighbours(image_id, neighbours):
+    global connection
+    statement = 'INSERT IGNORE INTO `image_neighbours` (`image_id`, `neighbours`) ' \
+                'VALUES (' + str(image_id) + ', "' + json.dumps(neighbours) + '");'
 
     cursor = connection.cursor()
     cursor.execute(statement)
--- group_images.py
+++ group_images.py
@@ -1,9 +1,8 @@
-import sys
-import os
-import pickle
-import json
-
 import numpy as np
+
+import db.mariadb as mariadb
+
+BATCH_SIZE = 50
 
 
 def calculate_euclidean_distance(matrix_a, matrix_b):
@@ -17,96 +16,47 @@ def calculate_euclidean_distance(matrix_a, matrix_b):
     return d
 
 
-def write_json(neighbours, output_path, file_name, neighbour_count):
+def start():
+    overall_features = mariadb.get_feature_count()
 
-    result = {
-        'file_name': file_name,
-        'neighbours': []
-    }
-    for name, distance in neighbours[0:neighbour_count]:
-        result['neighbours'].append({
-            'file_name': name,
-            'distances': float(distance)
-        })
+    offset = 0
+    while offset < overall_features:
 
-    with open(output_path, 'w') as output_file:
-        json.dump(result, output_file)
+        offset_comparing = 0
+        current_ids, current_features = mariadb.get_feature_batch(offset, BATCH_SIZE)
 
+        current_distances = None
+        current_compared_ids = []
 
-def write_pickle(neighbours, output_path):
-    with open(output_path, 'wb') as output_file:
-        pickle.dump(neighbours, output_file)
+        while offset_comparing < overall_features:
+            compared_ids, compared_features = mariadb.get_feature_batch(offset_comparing, BATCH_SIZE)
+            current_compared_ids.extend(compared_ids)
+            if current_distances is None:
+                current_distances = calculate_euclidean_distance(
+                    current_features,
+                    compared_features
+                )
+            else:
+                current_distances = np.append(
+                    current_distances,
+                    calculate_euclidean_distance(
+                        current_features,
+                        compared_features
+                    ),
+                    axis=1)
 
+            offset_comparing += BATCH_SIZE
 
-def _calculate_nearest_neighbours(root_path, output_directory):
-    neighbour_count = 20
-    file_list = []
+        for idx, image_id in enumerate(current_ids):
+            neighbour_distances = current_distances[idx]
+            sorted_neighbour_indices = list(np.argsort(neighbour_distances))
+            sorted_neighbour_file_names = [current_compared_ids[index] for index in sorted_neighbour_indices]
+            result = list(zip(sorted_neighbour_file_names, current_distances[sorted_neighbour_indices].flatten().tolist()))
 
-    for batch_directory in os.listdir(root_path):
-        current_batch_directory = f'{root_path}/{batch_directory}'
-        if not os.path.isdir(current_batch_directory):
-            continue
-
-        print(f'Processing directory {current_batch_directory}.')
-
-        with open(f'{current_batch_directory}/image_mapping.json', 'r') as image_mapping_file, \
-                open(f'{current_batch_directory}/features.pickle', 'rb') as image_features_file:
-
-            image_mapping = json.load(image_mapping_file)
-            image_features = np.array(pickle.load(image_features_file))
-
-            all_distances = None
-            all_compared_images_names = []
-
-            for inner_batch_directory in os.listdir(root_path):
-                compared_images_batch_directory = f'{root_path}/{inner_batch_directory}'
-                print(f'Comparing to {compared_images_batch_directory}')
-                if not os.path.isdir(current_batch_directory):
-                    continue
-
-                with open(f'{compared_images_batch_directory}/image_mapping.json', 'r') as compared_images_mapping_file, \
-                        open(f'{compared_images_batch_directory}/features.pickle', 'rb') as compared_images_features_file:
-
-                    compared_images_features = np.array(pickle.load(compared_images_features_file))
-
-                    all_compared_images_names.extend(json.load(compared_images_mapping_file))
-
-                    if all_distances is None:
-                        all_distances = calculate_euclidean_distance(
-                            image_features[:, 1:],
-                            compared_images_features[:, 1:]
-                        )
-                    else:
-                        all_distances = np.append(
-                            all_distances,
-                            calculate_euclidean_distance(
-                                image_features[:, 1:],
-                                compared_images_features[:, 1:]
-                            ),
-                            axis=1)
-
-            print(f'Writing batch results to disk.')
-
-            for idx, image_name in enumerate(image_mapping):
-                batch_output_path = f'{output_directory}/{batch_directory}/'
-                if not os.path.exists(os.path.dirname(batch_output_path)):
-                    os.makedirs(os.path.dirname(batch_output_path))
-
-                neighbours = all_distances[idx]
-                sorted_neighbour_indices = np.argsort(neighbours)
-                sorted_neighbour_file_names = [all_compared_images_names[index] for index in list(sorted_neighbour_indices)]
-                result = list(zip(sorted_neighbour_file_names, neighbours[sorted_neighbour_indices]))
-
-                write_pickle(result, f'{batch_output_path}{image_name}.neighbours.pickle')
-                write_json(result, f'{batch_output_path}{image_name}.neighbours.json', image_name, neighbour_count)
-
-                file_list.append(f'{batch_output_path}{image_name}')
-
-    with open(f'{output_directory}/images_list.json', 'w') as image_list_file:
-        json.dump(file_list, image_list_file)
-
-    print('Done.')
+            mariadb.write_neighbours(image_id, result)
+        offset += BATCH_SIZE
 
 
 if __name__ == '__main__':
-    _calculate_nearest_neighbours(root_path=sys.argv[1], output_directory=sys.argv[2])
+    mariadb.establish_connection()
+    start()
