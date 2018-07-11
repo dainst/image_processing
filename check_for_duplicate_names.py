969dbb2bbdac6ebde5ef56726d62e8d8421f8718
Simon Hohl <simon.hohl@dainst.org>
2018-07-11 12:05:56 +0200

    Adjust neighbours script
    Now writing results to MariaDB instead to disk.

---
+++ check_for_duplicate_names.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import logging
+
+logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+if __name__ == '__main__':
+    image_root = sys.argv[1]
+
+    known_images = {}
+
+    logger.info('Starting to process images...')
+    for root, dirs, files in os.walk(image_root):
+        for file in files:
+            if file.endswith('.jpg'):
+                if file in known_images:
+                    logger.error(file)
+                    known_images[file] += [os.path.abspath(f"{root}/{file}")]
+                else:
+                    known_images[file] = [os.path.abspath(f"{root}/{file}")]
+
+    for file in known_images:
+        if len(known_images[file]) != 1:
+            logger.info(f'{len(known_images[file])} duplicates for file name {file}:')
+            for path in known_images[file]:
+                logger.info(f'  {path}')
+
+    logger.info('Done.')
