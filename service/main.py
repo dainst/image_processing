fdd3e49a3b714e9bc22de3f4472c93dd41f9aef2
Simon Hohl <simon.hohl@dainst.org>
2018-07-12 09:37:08 +0200

    Add route for querying image name and its neighbours

--- service/main.py
+++ service/main.py
@@ -1,16 +1,26 @@
-
 from flask import Flask, jsonify, request, url_for
 from flask_cors import CORS
+import db.mariadb as mariadb
 
 app = Flask('nlp_service')
-cors = CORS(app, resources={r"*": {"origins": "http://localhost*"}})
+cors = CORS(app, resources={r'*': {'origins': 'http://localhost*'}})
 app.debug = True
+
+mariadb.establish_connection('127.0.0.1', 'main_user', 'pwd', 3308)
 
 
 @app.route('/')
 def index():
     app.logger.debug('Hello Wolfgang! Says the debug logger...')
     return 'Hello Wolfgang!'
+
+
+@app.route('/image/<image_id>', methods=['GET'])
+def image(image_id):
+    app.logger.debug(f'Image  requested.')
+    query_result = mariadb.get_image_and_neigbours_by_id(image_id)
+
+    return jsonify(query_result)
 
 
 if __name__ == '__main__':
