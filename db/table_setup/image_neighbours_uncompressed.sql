444ec4344af5684e087b9ace09a93b0d9c34bd9e
Simon Hohl <simon.hohl@dainst.org>
2019-03-26 14:34:24 +0100

    Add table for neighbours as evaluated using the uncompressed features

---
+++ db/table_setup/image_neighbours_uncompressed.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `image_neighbours_uncompressed` (
+   `image_id` int(10) unsigned NOT NULL,
+   `neighbours` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+   PRIMARY KEY (`image_id`),
+   UNIQUE KEY `image_id_UNIQUE` (`image_id`),
+   CONSTRAINT `fk_image_neighbours_uncompressed_to_image_files` FOREIGN KEY (`image_id`) REFERENCES `image_files` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
