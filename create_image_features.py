0d000d3569f2c10ba42c050d5b7dae859d516800
Simon Hohl <simon.hohl@dainst.org>
2019-03-26 15:04:03 +0100

    "Deactivate" old scripts
    Use Jupyter notebooks for the time being.

--- create_image_features.py
+++ create_image_features.py
@@ -1,102 +1,102 @@
-import sys
-import os
-import logging
-import db.mariadb as mariadb
-
-import tensorflow as tf
-import numpy as np
-
-logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-
-def create_graph():
-    with tf.gfile.FastGFile('./model/classify_image_graph_def.pb', 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-        _ = tf.import_graph_def(graph_def, name='')
-
-
-def already_processed(file_path, connection):
-    file_name = os.path.basename(file_path)
-    result = mariadb.get_features_by_name(file_name, connection)
-    return result is not None
-
-
-def process_file_list(path_list, connection):
-
-    with tf.Session() as sess:
-        for image_idx, image in enumerate(path_list):
-            try:
-                if image_idx % 1000 == 0:
-                    logger.info(f'Processed {image_idx} of {len(path_list)}.')
-
-                # Already processed images (= images with an associated feature-vector) are skipped
-                if already_processed(image, connection):
-                    continue
-
-                logger.debug(f'Parsing {image}, (#{image_idx})...')
-                if not tf.gfile.Exists(image):
-                    tf.logging.fatal(f'File does not exist {image}.')
-
-                with tf.gfile.FastGFile(image, 'rb') as f:
-                    image_data = f.read()
-
-                    feature_tensor = sess.graph.get_tensor_by_name('pool_3:0')
-                    feature_set = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
-                    feature_vector = np.squeeze(feature_set)
-
-                    mariadb.write_file_features(os.path.basename(image), feature_vector, connection)
-
-            except Exception as e:
-                logger.error(e)
-                logger.error(image)
-
-
-if __name__ == '__main__':
-
-    if len(sys.argv) != 7:
-        logger.info('Please provide as arguments: ')
-        logger.info(' 1) Path to image root directory.')
-        logger.info(' 2) MariaDB host.')
-        logger.info(' 3) MariaDB port.')
-        logger.info(' 4) MariaDB database name.')
-        logger.info(' 5) MariaDB user.')
-        logger.info(' 6) MariaDB password.')
-        sys.exit()
-
-    image_root = sys.argv[1]
-
-    connection = mariadb.get_connection(
-        host=sys.argv[2],
-        port=int(sys.argv[3]),
-        db_name=sys.argv[4],
-        user=sys.argv[5],
-        password=sys.argv[6]
-    )
-
-    path_list = []
-    file_list = []
-
-    image_counter = 0
-    batch_counter = 0
-
-    logger.info('Creating graph.')
-    create_graph()
-
-    logger.info(f'Collecting JPEGs in directory {image_root}.')
-    for root, dirs, files in os.walk(image_root):
-        for file in files:
-            if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.JPG') or file.endswith('JPEG'):
-                path_list.append(os.path.abspath(f'{root}/{file}'))
-                file_list.append(file)
-
-    logger.info('Writing file names to database.')
-    mariadb.write_file_names(file_list=file_list, connection=connection)
-
-    logger.info('Starting to process images...')
-    process_file_list(path_list, connection)
-    logger.info('Done.')
-
-    connection.close()
+# import sys
+# import os
+# import logging
+# import db.mariadb as mariadb
+#
+# import tensorflow as tf
+# import numpy as np
+#
+# logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+#
+#
+# def create_graph():
+#     with tf.gfile.FastGFile('./model/classify_image_graph_def.pb', 'rb') as f:
+#         graph_def = tf.GraphDef()
+#         graph_def.ParseFromString(f.read())
+#         _ = tf.import_graph_def(graph_def, name='')
+#
+#
+# def already_processed(file_path, connection):
+#     file_name = os.path.basename(file_path)
+#     result = mariadb.get_features_by_name(file_name, connection)
+#     return result is not None
+#
+#
+# def process_file_list(path_list, connection):
+#
+#     with tf.Session() as sess:
+#         for image_idx, image in enumerate(path_list):
+#             try:
+#                 if image_idx % 1000 == 0:
+#                     logger.info(f'Processed {image_idx} of {len(path_list)}.')
+#
+#                 # Already processed images (= images with an associated feature-vector) are skipped
+#                 if already_processed(image, connection):
+#                     continue
+#
+#                 logger.debug(f'Parsing {image}, (#{image_idx})...')
+#                 if not tf.gfile.Exists(image):
+#                     tf.logging.fatal(f'File does not exist {image}.')
+#
+#                 with tf.gfile.FastGFile(image, 'rb') as f:
+#                     image_data = f.read()
+#
+#                     feature_tensor = sess.graph.get_tensor_by_name('pool_3:0')
+#                     feature_set = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
+#                     feature_vector = np.squeeze(feature_set)
+#
+#                     mariadb.write_file_features(os.path.basename(image), feature_vector, connection)
+#
+#             except Exception as e:
+#                 logger.error(e)
+#                 logger.error(image)
+#
+#
+# if __name__ == '__main__':
+#
+#     if len(sys.argv) != 7:
+#         logger.info('Please provide as arguments: ')
+#         logger.info(' 1) Path to image root directory.')
+#         logger.info(' 2) MariaDB host.')
+#         logger.info(' 3) MariaDB port.')
+#         logger.info(' 4) MariaDB database name.')
+#         logger.info(' 5) MariaDB user.')
+#         logger.info(' 6) MariaDB password.')
+#         sys.exit()
+#
+#     image_root = sys.argv[1]
+#
+#     connection = mariadb.get_connection(
+#         host=sys.argv[2],
+#         port=int(sys.argv[3]),
+#         db_name=sys.argv[4],
+#         user=sys.argv[5],
+#         password=sys.argv[6]
+#     )
+#
+#     path_list = []
+#     file_list = []
+#
+#     image_counter = 0
+#     batch_counter = 0
+#
+#     logger.info('Creating graph.')
+#     create_graph()
+#
+#     logger.info(f'Collecting JPEGs in directory {image_root}.')
+#     for root, dirs, files in os.walk(image_root):
+#         for file in files:
+#             if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.JPG') or file.endswith('JPEG'):
+#                 path_list.append(os.path.abspath(f'{root}/{file}'))
+#                 file_list.append(file)
+#
+#     logger.info('Writing file names to database.')
+#     mariadb.write_file_names(file_list=file_list, connection=connection)
+#
+#     logger.info('Starting to process images...')
+#     process_file_list(path_list, connection)
+#     logger.info('Done.')
+#
+#     connection.close()
--- scan_files.py
+++ scan_files.py
@@ -1,86 +1,86 @@
-import argparse
-import logging
-import db.mariadb as mariadb
-import os
-import json
-import MySQLdb._exceptions as mysql_exceptions
-
-from requests_futures.sessions import FuturesSession
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
-
-parser = argparse.ArgumentParser(description='Scans a given directory and all its subdirectories for images. '
-                                             'Writes image names and paths to MariaDB.')
-
-parser.add_argument("dir", help="Directory to recursively search for images.")
-parser.add_argument("host", help="MariaDB host.")
-parser.add_argument("port", help="MariaDB port.")
-parser.add_argument("db", help="MariaDB database.")
-parser.add_argument("user", help="MariaDB user.")
-parser.add_argument("password", help="MariaDB user password")
-parser.add_argument("--arachne", help="Try retrieving Arachne URLs for image names (https://arachne.dainst.org)",
-                    action="store_true")
-
-
-def scan(image_directory, db_host, db_port, db_name, db_user, db_password , resolve_arachne_url):
-
-    data = []
-
-    logger.info(f'Collecting JPEGs in directory {image_directory}.')
-    for root, dirs, files in os.walk(image_directory):
-        for file in files:
-            if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.JPG') or file.endswith('JPEG'):
-                data.append((file, os.path.abspath(f'{root}/{file}'), None))
-
-    batch_counter = 0
-    batch_size = 1000
-
-    logger.info('Processing batches.')
-    if resolve_arachne_url:
-        logger.info('  Trying to resolve Arachne URLs, this may take some time.')
-
-    while batch_counter < len(data):
-
-        logger.info(f"  Processing batch {batch_counter} to {batch_counter+batch_size} of {len(data)}.")
-        current_batch = data[batch_counter:batch_counter+batch_size]
-
-        if resolve_arachne_url:
-
-            session = FuturesSession(max_workers=10)
-
-            arachne_path = 'https://arachne.dainst.org/data'
-            futures = []
-            for (name, _path, _url) in current_batch:
-                futures.append(session.get(f'{arachne_path}/search?q={name}'))
-
-            for idx, response in enumerate(futures):
-
-                try:
-                    entity_id = None
-                    json_value = response.result().json()
-                    if json_value['size'] == 1:
-                        entity_id = json_value['entities'][0]['entityId']
-
-                    if entity_id is not None:
-                        current_batch[idx] = (current_batch[idx][0], current_batch[idx][1],
-                                              f'{arachne_path}/image/{entity_id}')
-                except json.decoder.JSONDecodeError as e:
-                    logger.error(e)
-                    logger.error(response)
-
-        con = mariadb.get_connection(db_host, db_port, db_name, db_user, db_password)
-        try:
-            mariadb.write_files_data(current_batch, con)
-        except mysql_exceptions.DataError as e:
-            logger.error(e)
-        con.close()
-
-        batch_counter += batch_size
-
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    scan(args.dir, args.host, int(args.port), args.db, args.user, args.password, args.arachne is True)
+# import argparse
+# import logging
+# import db.mariadb as mariadb
+# import os
+# import json
+# import MySQLdb._exceptions as mysql_exceptions
+#
+# from requests_futures.sessions import FuturesSession
+#
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
+#
+# parser = argparse.ArgumentParser(description='Scans a given directory and all its subdirectories for images. '
+#                                              'Writes image names and paths to MariaDB.')
+#
+# parser.add_argument("dir", help="Directory to recursively search for images.")
+# parser.add_argument("host", help="MariaDB host.")
+# parser.add_argument("port", help="MariaDB port.")
+# parser.add_argument("db", help="MariaDB database.")
+# parser.add_argument("user", help="MariaDB user.")
+# parser.add_argument("password", help="MariaDB user password")
+# parser.add_argument("--arachne", help="Try retrieving Arachne URLs for image names (https://arachne.dainst.org)",
+#                     action="store_true")
+#
+#
+# def scan(image_directory, db_host, db_port, db_name, db_user, db_password , resolve_arachne_url):
+#
+#     data = []
+#
+#     logger.info(f'Collecting JPEGs in directory {image_directory}.')
+#     for root, dirs, files in os.walk(image_directory):
+#         for file in files:
+#             if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.JPG') or file.endswith('JPEG'):
+#                 data.append((file, os.path.abspath(f'{root}/{file}'), None))
+#
+#     batch_counter = 0
+#     batch_size = 1000
+#
+#     logger.info('Processing batches.')
+#     if resolve_arachne_url:
+#         logger.info('  Trying to resolve Arachne URLs, this may take some time.')
+#
+#     while batch_counter < len(data):
+#
+#         logger.info(f"  Processing batch {batch_counter} to {batch_counter+batch_size} of {len(data)}.")
+#         current_batch = data[batch_counter:batch_counter+batch_size]
+#
+#         if resolve_arachne_url:
+#
+#             session = FuturesSession(max_workers=10)
+#
+#             arachne_path = 'https://arachne.dainst.org/data'
+#             futures = []
+#             for (name, _path, _url) in current_batch:
+#                 futures.append(session.get(f'{arachne_path}/search?q={name}'))
+#
+#             for idx, response in enumerate(futures):
+#
+#                 try:
+#                     entity_id = None
+#                     json_value = response.result().json()
+#                     if json_value['size'] == 1:
+#                         entity_id = json_value['entities'][0]['entityId']
+#
+#                     if entity_id is not None:
+#                         current_batch[idx] = (current_batch[idx][0], current_batch[idx][1],
+#                                               f'{arachne_path}/image/{entity_id}')
+#                 except json.decoder.JSONDecodeError as e:
+#                     logger.error(e)
+#                     logger.error(response)
+#
+#         con = mariadb.get_connection(db_host, db_port, db_name, db_user, db_password)
+#         try:
+#             mariadb.write_files_data(current_batch, con)
+#         except mysql_exceptions.DataError as e:
+#             logger.error(e)
+#         con.close()
+#
+#         batch_counter += batch_size
+#
+#
+# if __name__ == '__main__':
+#     args = parser.parse_args()
+#     scan(args.dir, args.host, int(args.port), args.db, args.user, args.password, args.arachne is True)
