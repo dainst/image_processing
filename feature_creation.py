facc02318b913006cee7b061d536c873d6b4d396
Simon Hohl <simon.hohl@dainst.org>
2019-05-22 15:18:11 +0200

    Add argument handling for user credentials

--- feature_creation.py
+++ feature_creation.py
@@ -1,61 +1,93 @@
 # RUN AUTOENCODER JUPYTER NOTEBOOK FIRST, this will create the `encoder.h5` file used below
 
+import argparse
+import logging
 import keras
-from keras.models import load_model
-
-from keras.models import Model
-from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input
-from PIL.Image import DecompressionBombError
-import db.mariadb as mariadb
 import numpy as np
 
-print("Loading models...")
+from keras.models import load_model, Model
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input
+import keras.backend as K
+from PIL.Image import DecompressionBombError
 
-res_net = keras.applications.resnet50.ResNet50(include_top=False, pooling='avg')
+import db.mariadb as mariadb
 
-encoder = load_model('model/encoder.h5')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(format="%(asctime)s-%(levelname)s-%(name)s - %(message)s")
 
-print("Combining models...")
-combined = Model(inputs=res_net.input, outputs=encoder(res_net.output))
+parser = argparse.ArgumentParser(description="Create features for files in MariaDB.")
+parser.add_argument('model_path', help="path to keras model.")
+parser.add_argument('db_host', help="specify mariadb host.")
+parser.add_argument('db_port', type=int, help="specify mariadb port.")
+parser.add_argument('db_name', help="specify mariadb database.")
+parser.add_argument('db_user', help="specify mariadb user.")
+parser.add_argument('db_password', help="specify mariadb password.")
 
-con = mariadb.get_connection("127.0.0.1", 3308, "image_processing", "user", "user_pw")
 
-files_batch = mariadb.get_all_files_data(con)
+def euclidean_distance_loss(y_true, y_pred):
+    """
+    Euclidean distance loss
+    https://en.wikipedia.org/wiki/Euclidean_distance
+    :param y_true: TensorFlow/Theano tensor
+    :param y_pred: TensorFlow/Theano tensor of the same shape as y_true
+    :return: float
+    """
+    return K.sqrt(K.sum(K.square(y_pred - y_true), axis=-1))
 
-features = []
-features_info = []
 
-# Create training and test features
-print("Creating image features using both models.")
-counter = 1
+def create_features(model_path, host, port, database, user, password):
+    logger.info("Loading models...")
+    res_net = keras.applications.resnet50.ResNet50(include_top=False, pooling='avg')
 
-for (img_id, name, path, url) in files_batch:
-    try:
-        if counter % 100 == 0:
-            print(f'Progress: {counter}/{len(files_batch)}')
+    encoder = load_model(model_path, custom_objects={'euclidean_distance_loss': euclidean_distance_loss})
 
-        img = image.load_img(path, target_size=(224, 224))
-        img_data = image.img_to_array(img)
-        img_data = np.expand_dims(img_data, axis=0)
-        img_data = preprocess_input(img_data)
+    logger.info("Combining models...")
+    combined = Model(inputs=res_net.input, outputs=encoder(res_net.output))
 
-        combined_feature = combined.predict(img_data)
-        combined_feature = np.array(combined_feature).flatten()
+    con = mariadb.get_connection(host, port, database, user, password)
 
-        res_net_feature = res_net.predict(img_data)
-        res_net_feature = np.array(res_net_feature).flatten()
+    files_batch = mariadb.get_all_files_data(con)
 
-        mariadb.write_file_features(img_id, combined_feature, con)
-        mariadb.write_uncompressed_file_features(img_id, res_net_feature, con)
+    # Create training and test features
+    logger.info("Creating image features using both models.")
+    counter = 1
 
-        counter += 1
+    for (img_id, name, path, url) in files_batch:
+        try:
+            if counter % 100 == 0:
+                logger.info(f'Progress: {counter}/{len(files_batch)}')
 
-    except OSError as e:
-        print(e)
-    except DecompressionBombError as e:
-        print(e)
-        print(path)
+            img = image.load_img(path, target_size=(224, 224))
+            img_data = image.img_to_array(img)
+            img_data = np.expand_dims(img_data, axis=0)
+            img_data = preprocess_input(img_data)
 
-con.close()
-print('Done.')
+            combined_feature = combined.predict(img_data)
+            combined_feature = np.array(combined_feature).flatten()
+
+            res_net_feature = res_net.predict(img_data)
+            res_net_feature = np.array(res_net_feature).flatten()
+
+            mariadb.write_file_features(img_id, combined_feature, con)
+            mariadb.write_uncompressed_file_features(img_id, res_net_feature, con)
+
+            counter += 1
+
+        except OSError as e:
+            logger.error(e)
+        except DecompressionBombError as e:
+            logger.error(e)
+            logger.error(path)
+
+    con.close()
+    logger.info('Done.')
+
+
+if __name__ == "__main__":
+    options = vars(parser.parse_args())
+
+    create_features(options['model_path'], options['db_host'], options['db_port'], options['db_name'],
+                    options['db_user'], options['db_password'])
