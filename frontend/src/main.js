207a0d1bddaefdc8e90987c485ada6c4f01e2232
Simon Hohl <simon.hohl@dainst.org>
2020-07-07 09:49:46 +0200

    Add Vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
-
+Vue.use(Vuex);
 new Vue({
   router,
   render: (h) => h(App),
